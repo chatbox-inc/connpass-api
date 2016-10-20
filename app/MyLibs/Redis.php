6fb1dd260b0fa376cc0a8a5c2c51a8e315c3a395
44x1carbon <b4079@oic.jp>
2016-10-20 14:50:12 +0900

    Redisクラス作成

---
+++ app/MyLibs/Redis.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\MyLibs;
+
+class Redis {
+	private static $client = null;
+	
+	function __construct() {
+		if(is_null(self::$client)) {
+			self::$client = new \Predis\Client();
+		}
+	}
+
+	function get($key) {
+		return json_decode(self::$client->get($key));
+	}
+
+	function set($key,$value) {
+		$json_string = json_encode($value);
+		self::$client->set($key,$json_string);
+	}
+};
