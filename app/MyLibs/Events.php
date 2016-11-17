294d88a58fc4111be8f3988a0a40965522f140e9
44x1carbon <b4079@oic.jp>
2016-11-17 16:11:38 +0900

    コメント追加&戻り値をEventsに

--- app/MyLibs/Events.php
+++ app/MyLibs/Events.php
@@ -7,21 +7,30 @@ class Events {
 		$this->events = $events;
 	}
 
-	public function diff(Array $events) {
+	/**
+	* EventsとEventsの差分を取る
+	*
+	* @param Events $events
+	* @return Events  
+	*/
+	public function diff(Events $events) {
 		$map_eventid = function($event){
-			
 			return $event['event_id'];
 		};
 		
 		$self_eventids = array_map($map_eventid, $this->events);
-		$arg_eventids = array_map($map_eventid, $events);
+		$arg_eventids = array_map($map_eventid, $events->events);
 		$eventid_diff = array_diff($arg_eventids, $self_eventids); 
-		$diff= array_filter($events,function($event) use($eventid_diff){
+		$diff= array_filter($events->events,function($event) use($eventid_diff){
 			return in_array($event["event_id"],$eventid_diff);
 		});
 
-		return $diff;
-	} 
+		return new Events($diff);
+	}
+
+	public function filter(callable $cb) {
+		return array_filter($this->$events, $cb);
+	}
 }
 
 ?>
