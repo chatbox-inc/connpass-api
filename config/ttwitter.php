442a4a8cb84b40cf1405d1e8f3d226978726b9ca
44x1carbon <b4079@oic.jp>
2016-11-24 14:20:41 +0900

    env

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -12,10 +12,10 @@ return [
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
-    'CONSUMER_KEY'        => 'cAzeWs8jXYpTKYZrVbKdQQzc0',
-    'CONSUMER_SECRET'     => 'MoAQ7fbDlYIwPe6tdkUPzMscGd7yyL4O1I2Tx4Ex7o4OuFAh2I',
-    'ACCESS_TOKEN'        => '710253917867614208-xgG2jKtPr6427zAVl8bF886y2YFUAUG', 
-    'ACCESS_TOKEN_SECRET' => 'cnyYrNGk7jU60E62SAcAm3ZQLdrNCcbY9m5vnvzqr8Kr5',     
+    'CONSUMER_KEY'        => env('TWITTER_CONSUMER_KEY'),
+    'CONSUMER_SECRET'     => env('TWITTER_CONSUMER_SECRET'),
+    'ACCESS_TOKEN'        => env('TWITTER_ACCESS_TOKEN'), 
+    'ACCESS_TOKEN_SECRET' => env('TWITTER_ACCESS_TOKEN_SECRET'),     
 ];
 
 ?>
